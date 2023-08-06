c8fda4b7f1609319cc5409357b07eaf80886a6ce
rajnibala <rajni4265@gmail.com>
2023-08-06 15:59:31 +0000

    added bg and menu baar

--- app.py
+++ app.py
@@ -1,10 +1,7 @@
 from flask import Flask, render_template
-
 app = Flask(__name__)
-
 @app.route("/")
 def hello_world():
- return "hello "
-  
+ return render_template('Home.html')
 if __name__ == "__main__":
   app.run(host='0.0.0.0', debug=True)
