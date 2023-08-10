4c909e5b699afc2c9cb3807e2297d9a887eca582
rajnibala <rajni4265@gmail.com>
2023-08-10 18:00:41 +0000

    aading features jobs etc

--- app.py
+++ app.py
@@ -1,24 +1,35 @@
 from flask import Flask, render_template
 app = Flask(__name__)
-programs = [
+JOBS = [
   {
-    'id':1,
-    'title' : 'Importence of Meditation'
-    },
-  {
-    'id':2,
-    'title' : 'Importence of Meditation'
+    'id': 1,
+    'title': 'Data Analyst',
+    'location': 'Bengaluru, India',
+    'salary': 'Rs. 10,00,000'
   },
   {
-   'id':3,
-    'title' : 'Importence of Meditation' 
+    'id': 2,
+    'title': 'Data Scientist',
+    'location': 'Delhi, India',
+    'salary': 'Rs. 15,00,000'
+  },
+  {
+    'id': 3,
+    'title': 'Frontend Engineer',
+    'location': 'Remote'
+  },
+  {
+    'id': 4,
+    'title': 'Backend Engineer',
+    'location': 'San Francisco, USA',
+    'salary': '$150,000'
   }
 ]
 
-
-
 @app.route("/")
 def hello_world():
- return render_template('Home.html', programs=programs)
+    return render_template('home.html', 
+                           jobs=JOBS)
+
 if __name__ == "__main__":
   app.run(host='0.0.0.0', debug=True)
