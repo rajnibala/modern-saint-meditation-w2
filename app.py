64ab5f7cd1ca06387a26e1ed04ddef49454e86aa
rajnibala <rajni4265@gmail.com>
2023-08-10 17:24:05 +0000

    aaaaaaaaaaa

--- app.py
+++ app.py
@@ -1,7 +1,24 @@
 from flask import Flask, render_template
 app = Flask(__name__)
+programs = [
+  {
+    'id':1,
+    'title' : 'Importence of Meditation'
+    },
+  {
+    'id':2,
+    'title' : 'Importence of Meditation'
+  },
+  {
+   'id':3,
+    'title' : 'Importence of Meditation' 
+  }
+]
+
+
+
 @app.route("/")
 def hello_world():
- return render_template('Home.html')
+ return render_template('Home.html', programs=programs)
 if __name__ == "__main__":
   app.run(host='0.0.0.0', debug=True)
