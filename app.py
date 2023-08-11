12fe7b56416d5d9275836e522fdf5a48e33b5c1b
rajnibala <rajni4265@gmail.com>
2023-08-11 10:43:52 +0000

    added programs

--- app.py
+++ app.py
@@ -1,35 +1,39 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, jsonify
 app = Flask(__name__)
-JOBS = [
+PROGRAMS = [
   {
     'id': 1,
-    'title': 'Data Analyst',
-    'location': 'Bengaluru, India',
-    'salary': 'Rs. 10,00,000'
+    'title': 'Program1',
+    'Package': 'Bengaluru, India',
+    'Fees': 'Rs. 10,00,000'
   },
   {
     'id': 2,
-    'title': 'Data Scientist',
-    'location': 'Delhi, India',
-    'salary': 'Rs. 15,00,000'
+    'title': 'Program2',
+    'Package': 'Delhi, India',
+    'Fees': 'Rs. 15,00,000'
   },
   {
     'id': 3,
-    'title': 'Frontend Engineer',
-    'location': 'Remote'
+    'title': 'Program3',
+    'Package': 'Remote'
   },
   {
     'id': 4,
-    'title': 'Backend Engineer',
-    'location': 'San Francisco, USA',
-    'salary': '$150,000'
+    'title': 'Programs4',
+    'Package': 'San Francisco, USA',
+    'Fees': '$150,000'
   }
 ]
 
 @app.route("/")
-def hello_world():
-    return render_template('home.html', 
-                           jobs=JOBS)
+def hello_MSM():
+    return render_template('Home.html',Programs=PROGRAMS, company_name='MSM')
+
+@app.route("/api/Programs")
+def list_Programs():
+    return jsonify(PROGRAMS)
+
 
 if __name__ == "__main__":
   app.run(host='0.0.0.0', debug=True)
