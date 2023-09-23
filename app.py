6e478048a04b9dc8458004f084586bd389d66b6e
rajnibala <rajni4265@gmail.com>
2023-09-23 15:19:33 +0000

    database

--- app.py
+++ app.py
@@ -1,17 +1,18 @@
 from flask import Flask, render_template, jsonify
-from database import load_programs_from_db
+from database import load_program_from_db
+
 
 app = Flask(__name__)
 
 @app.route("/")
 def hello_MSM():
-    programs = load_programs_from_db()
-    return render_template('Home.html', programs=programs, company_name='MSM')
+    program = load_program_from_db()
+    return render_template('Home.html', program=program, company_name='MSM')
 
-@app.route("/api/Programs")
-def list_Programs():
-    programs = load_programs_from_db()  # Load programs from the database
-    return jsonify(programs)
+@app.route("/api/program")
+def list_program():
+    program = load_program_from_db()  # Load programs from the database
+    return jsonify(program)
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', debug=True)
--- database.py
+++ database.py
@@ -1,14 +1,16 @@
 from sqlalchemy import create_engine, text
 
-db_connection_string = "mysql+pymysql://svnb48ooni63f7o22ofe:pscale_pw_xSUBDmAUVN76fpARnZ83gtiplx4mW5vbnsbH0oV2S7@aws.connect.psdb.cloud/modernsaintmeditation?charset=utf8mb4"
+db_connection_string = "mysql+pymysql://yrpvqouzymlz00makhh9:pscale_pw_iSPBfxF58hLdpWD6Tmlem4rXtFeQMOH3WP0zGnXjGlf@aws.connect.psdb.cloud/meditation?charset=utf8mb4"
 
 engine = create_engine(db_connection_string, connect_args={
-  "ssl" : {
- "ssl_ca": "/home/gord/client-ssl/ca.pem"
-    
-  }
+    "ssl": {
+        "ssl_ca": "/etc/ssl/cert.pem"
+    }
 })
 
- with engine.connect() as conn:
-    result = conn.execute(text("select * from program"))
-   print(result.all())
+def load_program_from_db():
+    with engine.connect() as conn:
+        result = conn.execute(text("SELECT * FROM program"))
+        program = [dict(row) for row in result.all()]
+        return program
+
